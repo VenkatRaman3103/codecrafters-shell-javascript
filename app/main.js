bfc6155a4b9a1c2dad315f88502f1bad5c3cefc3
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-14 18:06:36 +0530

    update

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
-const readline = require("readline");
+import readline from "readline";
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 // Uncomment this block to pass the first stage
-// rl.question("$ ", (answer) => {
-//   rl.close();
-// });
+rl.question("$ ", (answer) => {
+    rl.close();
+});
